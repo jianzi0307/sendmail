dc22a20afe089c2ea44d229bd87a603acd55f329
jianzi0307 <jianzi0307@qq.com>
2019-11-15 18:47:36 +0800

    test: email

---
+++ app/admin/controller/EmailController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jianzi0307
+ * Date: 2019/11/15
+ * Time: 18:42
+ */
+
+namespace app\admin\controller;
+
+
+use cmf\controller\HomeBaseController;
+
+class EmailController extends HomeBaseController
+{
+    public function index()
+    {
+        return $this->fetch(':index');
+    }
+}
