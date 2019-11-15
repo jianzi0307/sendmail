de1b4080492cd7ecc3aa6fbf36b9e6f2aaa5ba73
jianzi0307 <jianzi0307@qq.com>
2019-11-15 20:00:19 +0800

    feat: email queue page.

---
+++ app/admin/lang/admin_menu.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jianzi0307
+ * Date: 2019/11/15
+ * Time: 19:57
+ */
+return [
+    'ADMIN_EMAIL_QUEUE' => '队列管理',
+    'ADMIN_EMAIL_QUEUE_ADD' => '添加队列',
+    'ADMIN_EMAIL_TEMPLATE' => '邮件模板'
+];
