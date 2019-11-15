d0aeaeb15e31d0fe409500376e8cc5b361b4892d
jianzi0307 <jianzi0307@qq.com>
2019-11-15 20:30:16 +0800

    fix

--- app/admin/lang/zh-cn/admin_menu.php
+++ app/admin/lang/zh-cn/admin_menu.php
@@ -6,7 +6,8 @@
  * Time: 19:57
  */
 return [
-    'ADMIN_EMAIL_QUEUE' => '邮件列表',
-    'ADMIN_EMAIL_QUEUE_ADD' => '添加队列',
-    'ADMIN_EMAIL_TEMPLATE' => '邮件模板'
+    'ADMIN_EMAIL_LIST' => '邮件列表',
+    'ADMIN_EMAIL_LIST_ADD' => '添加队列',
+    'ADMIN_EMAIL_TEMPLATE_LIST' => '邮件模板',
+    'ADMIN_EMAIL_TEMPLATE_ADD' => '添加模板'
 ];
